cf4af8400c4ed1ab8ec01157110c7deeabb474a4
Krisna Renaldi <krisna.renaldi@gmail.com>
2025-10-08 20:11:12 +0700

    Add link Dashboard to auth user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -99,6 +99,13 @@ export default function Home() {
                 >
                   Sign In
                 </Link>
+              ):(
+                <Link
+                  href="/dashboard"
+                  className="bg-amber-600 text-white px-4 py-2 rounded-lg hover:bg-amber-700 transition-colors"
+                >
+                  Dashboard
+                </Link>
               )}
             </nav>
           </div>
