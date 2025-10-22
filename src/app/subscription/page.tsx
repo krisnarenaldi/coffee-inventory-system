96a75f9fd1a69a66a6154a15be274c8063329e48
Krisna Renaldi <krisna.renaldi@gmail.com>
2025-10-22 21:08:20 +0700

    fix cycle yearly prorate 2

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, Suspense } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
